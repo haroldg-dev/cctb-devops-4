3539f8e08fb49881a48f398aaa33109644a77538
Harold <haroooldg@gmail.com>
2025-01-16 20:44:42 -0500

    Hello message changed and port updated

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = 7000;
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello, This is the web server of Harold Gutierrez!");
 });
 
 app.get("/health", (req, res) => {
